25b748af15cba751f69fc16f8e1c1e611a376fd2
Nandlal Kumar <61709433+nandlalKumar-Code@users.noreply.github.com>
2022-10-27 10:17:06 +0530

    Time: 344 ms (71.64%), Space: 124.5 MB (5.02%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-         int prefSum = 0;
-  
-  unordered_map<int, int> mp;
-  for(int i=0; i<nums.size(); i++)
-  {
-    prefSum += nums[i];
-    prefSum %= k;
-
-    if(prefSum == 0 && i) return true;
-
-    if(mp.find(prefSum) != mp.end())  // Found the required prefix sum 
-    {
-      if(i - mp[prefSum] > 1) return true; // check if atleast 2 elements are there or not
-    }
-    else mp[prefSum] = i;
-  }
-
-  return false; 
+        int n=nums.size();
+        vector<int>prefixSum;
+        unordered_map<int,int>mp;
+        
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            prefixSum.push_back(sum);
+        }
+        
+        mp[0]=0;
+        for(int i=0;i<n;i++){
+            if(mp.count(prefixSum[i]%k)==0){
+                mp[prefixSum[i]%k]=i+1;
+            }
+            else if(mp[prefixSum[i]%k]<i){
+                return true;
+            }
+        }
+        return false;
         
     }
 };
