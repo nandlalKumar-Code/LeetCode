fd0bc6557f387c891332faf42ce13b9079c99f3e
Nandlal Kumar <61709433+nandlalKumar-Code@users.noreply.github.com>
2022-11-12 11:39:59 +0530

    Time: 339 ms (96.29%), Space: 117.2 MB (19.54%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -8,6 +8,19 @@ public:
         
     }
     
+    //logic
+    // ---|---|----|---x---|----|----|---
+    //here largest of smaller element i.e., <= x in put into maxHeap so that 
+    //we get the top element from left side
+    //smallest of largest element i.e., >x is put into minHeap so that
+    //we get the top element from right side
+    
+    //median state that elements must be ordered and 
+    //middle element of sorted integer gives us the median
+    //largest and smaller heap helps us to divide the integers
+    //into two halves and returns us middle element
+    
+    
     void addNum(int num) {
         if(largest.empty() || num <= largest.top()){
             largest.push(num);
