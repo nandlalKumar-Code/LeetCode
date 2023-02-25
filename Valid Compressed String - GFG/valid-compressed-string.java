13c6a3fbaef40b993a30cf81539b85905ba800f4
Nandlal Kumar <61709433+nandlalKumar-Code@users.noreply.github.com>
2023-02-25 20:26:23 +0530

    Added solution - LeetHub

--- Valid Compressed String - GFG/valid-compressed-string.java
+++ Valid Compressed String - GFG/valid-compressed-string.java
@@ -33,22 +33,32 @@ class Solution {
             if((T.charAt(i)>='0') && (T.charAt(i)<='9')){
                 num=(num*10)+(T.charAt(i)-'0');
                 
+                //when we encounter last element of T as numeric
                 if((i==nt-1) && (num>0)){
                     j+=num;
+                    
+                    //if j is not ns means some element of S string is left 
+                    //or some extra element has been deleted
                     if(j!=ns){
                         return 0;
                     }
                 }
                 
             }
+            //if both string has character at their current place
             else if(num==0){
                 if(S.charAt(j)!=T.charAt(i)){
                     return 0;
                 }
                 j++;
             }
+            
+            //num is not zero some part we have skip in S string
             else{
                 j+=num;
+                
+                //if j>=ns && i<nt-1 :: j is out index and i has some element to compare
+                //second condition :: after skipping the num elements character obtained is not equal
                 if(((j>=ns) && (i<nt-1)) || (S.charAt(j)!=T.charAt(i))){
                     return 0;
                 }
